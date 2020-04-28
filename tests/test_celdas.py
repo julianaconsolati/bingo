3ee65e562219e76152f3cca3b4e684592c4d0564
juliana <julianaconsolati@gmail.com>
2020-04-27 21:43:01 -0300

    Agrego funcion test_filas_ocupadas

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -9,10 +9,10 @@ def contar_celdas_ocupadas():
     return contador
 
 def test_menos_de_15():
-    assert test_contar_celdas_ocupadas() <= 15
+    assert contar_celdas_ocupadas() <= 15
 
 def test_mas_de_15():
-    assert test_contar_celdas_ocupadas() >= 15
+    assert contar_celdas_ocupadas() >= 15
 
 def test_columnas_ocupadas():
     mi_carton = carton()
@@ -22,6 +22,18 @@ def test_columnas_ocupadas():
             if mi_carton[j][i] == 0:
                 contador += 1
         if contador == 3:
+            assert False
+        contador = 0
+    assert True
+
+def test_filas_ocupadas():
+    mi_carton = carton()
+    contador = 0
+    for i in range(3):
+        for j in range(9):
+            if mi_carton[i][j] == 0:
+                contador += 1
+        if contador == 9:
             assert False
         contador = 0
     assert True
