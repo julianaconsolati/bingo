4b370a9a0c19c241aaac7b35454c17a39f8c8807
juliana <julianaconsolati@gmail.com>
2020-04-27 21:31:59 -0300

    Revert "comento las funciones" (la cague)
    This reverts commit 1db8e6852e3f1e42a4ec506d1b15fd374798e330.

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -1,6 +1,5 @@
 from src.bingo import carton
 
-# cuenta cuantas celdas ocupadas con 1s hay
 def contar_celdas_ocupadas():
     mi_carton = carton()
     contador = 0
@@ -9,15 +8,12 @@ def contar_celdas_ocupadas():
             contador += celda
     return contador
 
-# testea que el carton tenga 15 celdas ocupadas o menos
 def test_menos_de_15():
     assert test_contar_celdas_ocupadas() <= 15
 
-# testea que el carton tenga 15 celdas ocupadas o mas
 def test_mas_de_15():
     assert test_contar_celdas_ocupadas() >= 15
 
-# testea que todas las columnas tengan al menos una celda ocupada
 def test_columnas_ocupadas():
     mi_carton = carton()
     contador = 0
