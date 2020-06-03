94d30598f916e2336c4ad15fa76f65601ba8b4fb
Juliana <julianaconsolati@gmail.com>
2020-06-03 00:52:58 -0300

    arregle bingo.py

--- src/bingo.py
+++ src/bingo.py
@@ -1,5 +1,4 @@
 def carton():
-<<<<<<< HEAD
     cartonConCeldas = (
         (3,0,22,0,41,52,0,0,89),
         (7,12,0,38,0,57,0,73,0),
@@ -17,13 +16,6 @@ def sin_numeros_repeditos(mi_carton):
                         if celda == celda2:
                             return False
     return True
-=======
-    carton = (
-        (0,0,27,36,0,52,62,0,80),
-        (0,11,0,37,41,0,0,75,88),
-        (3,12,0,0,48,0,65,76,0)
-    )
-    return carton
 
 # Retorna True si las celdas ocupadas se encuentran entre 1 y 90, False en caso contrario
 def celdas_1_a_90(mi_carton):
@@ -32,4 +24,3 @@ def celdas_1_a_90(mi_carton):
             if(celda >= 0 and celda <= 0):
                 return True
     return False
->>>>>>> 80f312ff320f77429dc7ee3813c7e242078a5fb8
